e049aa81680c531445db2b3767870bffa3b3a3ef
Komalchaudhari-01 <chaudharikomal1903@gmail.com>
2026-03-30 22:31:39 +0530

    renamed

--- if_else_leap year.py
+++ if_else_leap year.py
@@ -2,8 +2,8 @@
 year = int(input("Enter a year"))
 
 if year % 100 == 0 and year % 400 == 0:
-    print(f"It/'s a leap year")
+    print(f"It's a leap year")
 elif year % 100 != 0 and year % 4 == 0:
     print(f"It's a leap year")
 else:
-    print(f"It/'s not a leap year")
+    print(f"It's not a leap year")
