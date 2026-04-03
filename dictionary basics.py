605bffdb80d45f8321d5cc087f028aacd302be35
Komalchaudhari-01 <chaudharikomal1903@gmail.com>
2026-04-03 22:06:11 +0530

    Basics of dictionary

---
+++ dictionary basics.py
@@ -0,0 +1,14 @@
+
+students = {"A": 10, "B": 20, "C": 30}
+
+# 1. Print keys
+for k in students:
+    print(k)
+
+# 2. Print values
+for v in students.values():
+    print(v)
+
+# 3. Print key-value pairs
+for k, v in students.items():
+    print(k, v)
---
+++ dictionary_max marks.py
@@ -0,0 +1,6 @@
+
+marks = {"Rahul": 45, "Amit": 78, "Neha": 32, "Priya": 90}
+
+for i in marks:
+    if marks[i] > 50:
+        print(i)
